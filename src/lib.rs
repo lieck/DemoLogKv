9eccdd3fb8d40a97ca2ec58143564fa6a82d44ef
lieck <728269388@qq.com>
2023-04-16 23:58:02 +0800

    pro2

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,103 @@
-use std::collections::HashMap;
+mod error;
+mod wal;
+
+pub use error::{KvsError, Result};
+use wal::WalLog;
+
+use std::{collections::HashMap, fs};
 
 pub struct KvStore {
-    kv: HashMap<String, String>,
+    kv: HashMap<String, u64>,
+    wal: WalLog,
+    count: u32,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore { kv: HashMap::new() }
+        let mut s = KvStore {
+            kv: HashMap::new(),
+            wal: WalLog::new(String::new()),
+            count: 0,
+        };
+        s.init();
+        s
+    }
+
+    pub fn init(&mut self) {
+        for e in self.wal.read_all().unwrap() {
+            match e.entry {
+                wal::LogEntry::Set { key, value: _ } => {
+                    self.kv.insert(key, e.idx);
+                }
+                wal::LogEntry::Rm { key } => {
+                    self.kv.remove(&key);
+                }
+            }
+        }
     }
 
     pub fn set(&mut self, key: String, val: String) {
-        self.kv.insert(key, val);
+        let pos = self
+            .wal
+            .write(&wal::LogEntry::Set {
+                key: key.clone(),
+                value: val,
+            })
+            .unwrap();
+        self.kv.insert(key, pos);
+
+        self.count += 1;
+        if self.count > 1000 {
+            self.wal.compacting();
+        }
     }
 
     pub fn get(&self, key: String) -> Option<String> {
         if self.kv.contains_key(&key) {
-            return Some(self.kv.get(&key).unwrap().clone());
+            let pos = self.kv.get(&key).unwrap();
+            let val = self.wal.read_line_at(*pos);
+
+            return match val {
+                Ok(val) => Some(val),
+                Err(KvsError::KeyNotFound) => None,
+                Err(err) => panic!("{}", err.to_string()),
+            };
         }
         None
     }
 
     pub fn remove(&mut self, key: String) {
         self.kv.remove(&key);
+        self.wal.write(&wal::LogEntry::Rm { key }).unwrap();
+
+        self.count += 1;
+        if self.count > 1000 {
+            self.wal.compacting();
+        }
+    }
+
+    pub fn compacting_wal_log(&mut self) {
+        self.count = 0;
+
+        let mut new_wal = WalLog::new(String::from("test.txt"));
+        let mut new_kv = HashMap::new();
+        for kv in &mut self.kv {
+            let value = self.wal.read_line_at(*kv.1).unwrap();
+            let pos = new_wal
+                .write(&wal::LogEntry::Set {
+                    key: kv.0.clone(),
+                    value,
+                })
+                .unwrap();
+            new_kv.insert(kv.0.clone(), pos);
+        }
+        self.kv = new_kv;
+        self.wal = new_wal;
+
+        // 更改文件名
+        fs::remove_file("log.txt").unwrap();
+        fs::rename("test.txt", "log.txt").unwrap();
+        self.wal.set_file_name("log.txt".to_string());
     }
 }
 
@@ -29,4 +105,27 @@ impl Default for KvStore {
     fn default() -> Self {
         Self::new()
     }
+}
+
+#[test]
+fn compacting_wal_log() {
+    let mut kvs = KvStore::new();
+    for i in 0..100 {
+        kvs.set(i.to_string(), i.to_string());
+    }
+    for i in 0..100 {
+        kvs.remove(i.to_string());
+    }
+    for i in 0..100 {
+        kvs.set(i.to_string(), i.to_string());
+    }
+    kvs.compacting_wal_log();
+    for i in 0..100 {
+        _ = kvs.get(i.to_string()).unwrap();
+    }
+}
+
+#[test]
+fn remove_file() {
+    fs::remove_file("log.txt").unwrap();
 }
